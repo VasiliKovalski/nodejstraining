c12927260ca8e23a61f8853eb305e60a26ca58d2
VasKovalski <vasili.kovalski@gmail.com>
2025-03-08 09:25:57 +1300

    5 records

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@
 exports.getUsers = async (req, res) => {
   try {
     const pool = await poolPromise;
-    const result = await pool.request().query("SELECT top 40 * FROM customers");
+    const result = await pool.request().query("SELECT top 5 * FROM customers");
     res.json(result.recordset);
   } catch (err) {
     res.status(500).json({ error: "Database query failed", details: err.message });
