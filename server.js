f7f9a3959813f14e2804ef32fe669b7e12047c8a
VasKovalski <vasili.kovalski@gmail.com>
2025-03-07 23:50:37 +1300

    Removed comments

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const port = process.env.PORT || 8080;
 require("dotenv").config(); // Load environment variables from .env
 
 
-//jopa 2134123123 sdfssddf sdfsfsdfsdfg hfghf ghfghdfg dfgd fgdfg
-
 require("dotenv").config(); // Load environment variables
 
 const config = {
@@ -78,12 +76,6 @@ app.get("/users", async (req, res) => {
 
 
 
-//module.exports = pool;
-
-
-
-
-  //const result = pool.query('select * from playing_with_neon');
 
 app.get('/api', (req, res) => { 
     res.json({"users": ["user_1999999999", "user_29999999", "user_39999999"]})
@@ -100,24 +92,3 @@ app.listen(port, () => {
 
 
 
-//const apiRoutes = require("./routes/apiRoutes");
-
-//app.use("/api", apiRoutes);
-
-//const mySum = require('../mySumFunc.cjs')
-//console.log('vasya')
-
-
-
-
-
-//const path = require('node:path')
-
-//console.log(__filename)
-//console.log(__dirname)
-
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASSWORD,
-//     port: process.env.DB_PORT || 5432,
